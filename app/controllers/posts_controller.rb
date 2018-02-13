d0642ebdacb72421b57da901b9b7bdb001ce5499
jtb1137 <jtb1137@gmail.com>
2018-02-13 14:19:41 -0500

    Basic CRUD functional

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
 
     def index
+        @posts = Post.all
     end
 
     def new
@@ -9,6 +10,12 @@ class PostsController < ApplicationController
 
     def create
         @post = Post.new(post_params)
+        
+        if @post.save
+            redirect_to post_path(@post)
+        else
+            render "new"
+        end
     end
 
     def show
@@ -20,9 +27,20 @@ class PostsController < ApplicationController
     end
 
     def update
+        @post = Post.find(params[:id])
+
+        if @post.update(post_params)
+            redirect_to post_path(@post)
+        else
+            render "edit"
+        end
     end
 
     def destroy
+        @post = Post.find(params[:id])
+        @post.destroy
+
+        redirect_to root_path
     end
 
     private
