f6a4dc6e75e6cdd129e0354bbea69ca16ab99592
jtb1137 <jtb1137@gmail.com>
2018-02-13 14:01:36 -0500

    Add basic  Posts routes & views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,32 @@
 class PostsController < ApplicationController
+
+    def index
+    end
+
+    def new
+        @post = Post.new
+    end
+
+    def create
+        @post = Post.new(post_params)
+    end
+
+    def show
+        @post = Post.find(params[:id])
+    end
+
+    def edit
+        @post = Post.find(params[:id])
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+    private
+    def post_params
+        params.require(:post).permit(:title, :description)
+    end
 end
