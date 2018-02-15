e552969fcb88a12c78a41e583b6a221b6af6d62c
jtb1137 <jtb1137@gmail.com>
2018-02-15 15:18:06 -0500

    Created Post Partial

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'posts#index'
   devise_for :users, :controllers => { registrations: 'registrations' }
   
-  resources :users, only: [:show]
+  resources :users
   
   resources :posts do
     resources :comments
