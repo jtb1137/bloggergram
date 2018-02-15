6acb2bcdfde0a633ab0efd19a7fcfb41ecbac8e8
jtb1137 <jtb1137@gmail.com>
2018-02-15 12:43:33 -0500

    DRYed Posts Controller & started User associations

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,20 +1,19 @@
 class PostsController < ApplicationController
+    before_action :authenticate_user!
+    before_action :set_post, only: [:show, :edit, :update, :destroy]
+    
     def index
         @posts = Post.all
     end
 
     def new
-        if user_signed_in?
-            @post = Post.new   
-        else
-            redirect_to new_user_session_path
-        end
+        @post = Post.new   
     end
 
     def create
         @post = Post.new(post_params)
         
-        if @post.save && user_signed_in?
+        if @post.save
             redirect_to post_path(@post)
         else
             render "new"
@@ -22,21 +21,15 @@ class PostsController < ApplicationController
     end
 
     def show
-        @post = Post.find(params[:id])
+
     end
 
     def edit
-        if user_signed_in?
-            @post = Post.find(params[:id])
-        else
-            redirect_to new_user_session_path
-        end
+
     end
 
     def update
-        @post = Post.find(params[:id])
-
-        if @post.update(post_params) && user_signed_in?
+        if @post.update(post_params)
             redirect_to post_path(@post)
         else
             render "edit"
@@ -44,19 +37,18 @@ class PostsController < ApplicationController
     end
 
     def destroy
-        if user_signed_in?
-            @post = Post.find(params[:id])
-            @post.comments.destroy_all
-            @post.destroy
+        @post.comments.destroy_all
+        @post.destroy
 
-            redirect_to root_path
-        else
-            redirect_to new_user_session_path
-        end
+        redirect_to root_path
     end
 
     private
     def post_params
         params.require(:post).permit(:image, :title, :description)
+    end
+
+    def set_post
+        @post = Post.find(params[:id])
     end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,10 @@
 class Post < ApplicationRecord
     has_attached_file :image, styles: { medium: "500x500", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+    
     validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
     validates_presence_of :image, :title, :description
     validates :title, length: { maximum: 100 }
+
     has_many :comments
+    belongs_to :user
 end
