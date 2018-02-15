863d11b6fb092e73e55507b267fa2c5a785df3d0
jtb1137 <jtb1137@gmail.com>
2018-02-15 14:48:57 -0500

    Add User show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root 'posts#index'
   devise_for :users, :controllers => { registrations: 'registrations' }
+  
+  resources :users, only: [:show]
+  
   resources :posts do
     resources :comments
   end
