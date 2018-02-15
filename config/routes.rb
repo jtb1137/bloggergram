ccd554dad26ac08f60aa805970964e8f5dca4979
jtb1137 <jtb1137@gmail.com>
2018-02-15 12:33:20 -0500

    New username field in Signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'posts#index'
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
   resources :posts do
     resources :comments
   end
